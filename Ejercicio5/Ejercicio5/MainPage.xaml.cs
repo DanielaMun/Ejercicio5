42c8a2fee4c89ccfc33497949b139b2b235ec0c9
DanielaMun <41449250+DanielaMun@users.noreply.github.com>
2018-08-30 13:31:22 -0500

    Initial commit

---
+++ Ejercicio5/Ejercicio5/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio5
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void btn1_Clicked(object sender, EventArgs e)
+        {
+            panel.Children.Add(new Button()
+            {
+                Text=$"Boton{ panel.Children.Count + 1 }"
+            });
+        }
+    }
+}
